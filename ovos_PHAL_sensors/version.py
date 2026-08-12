442b7ebbdd65717fbc3585015bde31cf17ae44c2
JarbasAl <33701864+JarbasAl@users.noreply.github.com>
2026-08-12 14:48:57 +0000

    Increment Version to 0.1.0a1

--- ovos_PHAL_sensors/version.py
+++ ovos_PHAL_sensors/version.py
@@ -1,9 +1,9 @@
 # The following lines are replaced during the release process.
 # START_VERSION_BLOCK
 VERSION_MAJOR = 0
-VERSION_MINOR = 0
+VERSION_MINOR = 1
 VERSION_BUILD = 0
-VERSION_ALPHA = 19
+VERSION_ALPHA = 1
 # END_VERSION_BLOCK
 
 __version__ = f"{VERSION_MAJOR}.{VERSION_MINOR}.{VERSION_BUILD}"
